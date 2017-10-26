89c0f1b6f2fcb9c7536639010ca53f09a1cc2b73
Cihan Bebek <cihan.m.bebek@gmail.com>
2017-10-26 17:58:37 +0300

    remove default project, add first component Courses

---
+++ src/components/Courses.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Courses.css';
+
+class Courses extends Component {
+  render() {
+    return (
+      <div className="Courses">
+      </div>
+    );
+  }
+}
+
+export default Courses;
