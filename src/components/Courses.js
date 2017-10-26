f6cef3556d26c922baf29db0067593e62971f723
Cihan Bebek <cihan.m.bebek@gmail.com>
2017-10-26 18:40:36 +0300

    render course names

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import '../css/Courses.css';
 
+var testCourses = [
+  {
+    code: "TIEP1",
+    name: "Lausekielinen Ohjelmointi I",
+    credits: 5,
+    url: "https://www10.uta.fi/opas/opetusohjelma/marjapuuro.htm?id=34877"
+  },
+  {
+    code: "TIEP3",
+    name: "Tietokantojen perusteet",
+    credits: 5,
+    url: "https://www10.uta.fi/opas/opetusohjelma/marjapuuro.htm?id=35528"
+  },
+  {
+    code: "FILP1A",
+    name: "Johdatus filosofiaan",
+    credits: 2,
+    url: "https://www10.uta.fi/opas/opetusohjelma/marjapuuro.htm?id=36647"
+  }
+]
+
 class Courses extends Component {
+
+  renderCourseListItem = (course) => {
+    return (<li>
+      { course.name }
+    </li>)
+  }
+
   render() {
     return (
       <div className="Courses">
+        <ul>
+          { testCourses.map((course) => this.renderCourseListItem(course)) }
+        </ul>
       </div>
     );
   }
