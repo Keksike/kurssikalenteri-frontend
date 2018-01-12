c1937ebf73eecf1e3714c851248f2ffedc16bbf1
Emma Hanhiniemi <emhanhin@Emma-MacBook-Pro.local>
2018-01-12 18:40:06 +0200

    Lisätty kalenteri-komponentti

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Courses from './components/Courses';
+import Calendar from './components/Calendar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Courses />
+        <Calendar />
       </div>
     );
   }
---
+++ src/components/Calendar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+  render() {
+    return (
+      <div className="Calendar"></div>
+    );
+  }
+}
+
+export default Calendar;
