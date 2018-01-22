05da387aad4c98e4a41b3aa1cd21be8e2498447c
hansupie <hannemarikuusisto@gmail.com>
2018-01-22 23:12:37 +0200

    styled CourseListItem and changed colours

--- src/components/CourseListItem.js
+++ src/components/CourseListItem.js
@@ -4,12 +4,14 @@ import '../css/CourseListItem.css';
 class CourseListItem extends Component {
   render() {
     return (
-      <li>
-        <button onClick={() => console.log("stringi vaan sinne")}>
-          <span className="course-name">{ this.props.course.name }</span>
-          <span className="course-credits">{ this.props.course.credits }</span>
-        </button>
-      </li>
+      <ul>
+        <li>
+          <button onClick={() => console.log("stringi vaan sinne")}>
+            <span className="course-name">{ this.props.course.name }</span>
+            <span className="course-credits">{ this.props.course.credits }</span>
+          </button>
+        </li>
+      </ul>
     );
   }
 }
