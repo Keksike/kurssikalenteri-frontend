9d25700ddb4960c5db6b27040f65ed60f71463bb
Cihan Bebek <cihan.m.bebek@gmail.com>
2018-01-23 18:36:34 +0200

    add react-big-calendar with some test events & minor style fixes

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+import '../css/Calendar.css';
+
+moment.locale('fi', {
+  week: {
+      dow: 1,
+      doy: 1,
+  },
+});
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
+
+const testEvents = [
+  {
+    id: 0,
+    title: 'Toinen tapahtuma',
+    start: new Date(2018, 0, 23, 16, 0, 0),
+    end: new Date(2018, 0, 23, 17, 0, 0)
+  },
+  {
+    id: 1,
+    title: 'Päivän tapahtuma',
+    allDay: true,
+    start: new Date(2018, 0, 21),
+    end: new Date(2018, 0, 22),
+  }
+];
 
 class Calendar extends Component {
+
   render() {
     return (
-      <div className="Calendar"></div>
+      <div className="Calendar">
+        <BigCalendar
+          views={['month', 'week']}
+          events={testEvents}
+          onSelectEvent={(event) => console.log(event.title)}
+        />
+      </div>
     );
   }
 }
